f34c4fa852a60e81c3281140efbbe68bb7fbfff2
MarcosFP97 <marcosfernandez.pichel@usc.es>
2023-10-02 16:01:32 +0200

    First sentence retrieval model evaluated

--- src/retrieval-beir.py
+++ src/retrieval-beir.py
@@ -1,3 +1,6 @@
+import argparse
+import json
+
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval import models
 from beir.retrieval.evaluation import EvaluateRetrieval
@@ -28,6 +31,12 @@ def evaluate_sentence_transf(
     return ndcg, _map, recall, precision
 
 if __name__=="__main__":
-    corpus,queries,qrels = load_custom_data("../dataset_format_beir/sentences.jsonl", "../dataset_format_beir/queries.jsonl", "../dataset_format_beir/qrels.tsv")
-    ndcg, _map, recall, precision = evaluate_sentence_transf('multi-qa-mpnet-base-dot-v1', corpus, queries, qrels)
-    print("Ndcg:", ndcg, "MAP:", _map, "Recall:", recall, "Precision:", precision)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", nargs='?', default="queries") ### With this param we select the kind of query: only the BDI item tite, the firs question, etc.
+    args = parser.parse_args()
+    corpus,queries,qrels = load_custom_data("../dataset_format_beir/sentences.jsonl", "../dataset_format_beir/"+str(args.query)+".jsonl", "../dataset_format_beir/qrels.tsv")
+    sr_model = 'multi-qa-mpnet-base-dot-v1'
+    ndcg, _map, recall, precision = evaluate_sentence_transf(sr_model, corpus, queries, qrels)
+    with open("../baselines/sentence_retrieval.txt",'a+') as f:
+        print("Ndcg:", ndcg, "MAP:", _map, "Recall:", recall, "Precision:", precision)
+        f.write("Model:"+sr_model+"\nNdcg:"+ json.dumps(ndcg)+ " MAP:"+ json.dumps(_map) + " Recall:"+ json.dumps(recall) + " Precision:"+ json.dumps(precision))
