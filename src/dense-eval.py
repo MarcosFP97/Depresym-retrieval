8e48329d73a39cd0b7dbbcbdc57745b03123949b
marcos.fernandez.pichel <marcosfernandez.pichel@usc.es>
2023-10-03 18:07:30 +0200

    error when loading DPR moedl

---
+++ src/dense-eval.py
@@ -0,0 +1,63 @@
+import argparse
+import json
+
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval import models
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+def load_custom_data(
+    corpus_path:str,
+    query_path:str,
+    qrels_path:str
+):
+    corpus, queries, qrels = GenericDataLoader(
+                                    corpus_file=corpus_path, 
+                                    query_file=query_path, 
+                                    qrels_file=qrels_path).load_custom()
+    
+    return corpus, queries, qrels
+
+def evaluate_dpr(
+    corpus:object,
+    queries:object,
+    qrels:object
+):
+    model = DRES(models.SentenceBERT((
+        "facebook-dpr-question_encoder-multiset-base",
+        "facebook-dpr-ctx_encoder-multiset-base"), batch_size=128))    
+    retriever = EvaluateRetrieval(model, score_function="dot")
+    results = retriever.retrieve(corpus, queries)
+    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+    return ndcg, _map, recall, precision
+
+#### Here include more methods for ANCE, etc.
+
+def evaluate_ance(
+    corpus:object,
+    queries:object,
+    qrels:object
+):
+    model = DRES(models.SentenceBERT("msmarco-roberta-base-ance-firstp"))
+    retriever = EvaluateRetrieval(model, score_function="dot")
+    results = retriever.retrieve(corpus, queries)
+    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+    return ndcg, _map, recall, precision
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", nargs='?', default="queries") ### With this param we select the kind of query: only the BDI item tite, the firs question, etc.
+    args = parser.parse_args()
+    corpus,queries,qrels = load_custom_data("../dataset_format_beir/sentences.jsonl", "../dataset_format_beir/"+str(args.query)+".jsonl", "../dataset_format_beir/qrels.tsv")
+    
+    #### DPR eval
+    ndcg, _map, recall, precision = evaluate_dpr(corpus, queries, qrels)
+    with open("../baselines/dpr.txt",'a+') as f:
+        print("Ndcg:", ndcg, "MAP:", _map, "Recall:", recall, "Precision:", precision)
+        f.write("\nNdcg:"+ json.dumps(ndcg)+ " MAP:"+ json.dumps(_map) + " Recall:"+ json.dumps(recall) + " Precision:"+ json.dumps(precision))
+    
+    #### ANCE eval
+    ndcg, _map, recall, precision = evaluate_ance(corpus, queries, qrels)
+    with open("../baselines/dpr.txt",'a+') as f:
+        print("Ndcg:", ndcg, "MAP:", _map, "Recall:", recall, "Precision:", precision)
+        f.write("\nNdcg:"+ json.dumps(ndcg)+ " MAP:"+ json.dumps(_map) + " Recall:"+ json.dumps(recall) + " Precision:"+ json.dumps(precision))
---
+++ src/semantic-search-eval.py
@@ -0,0 +1,42 @@
+import argparse
+import json
+
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval import models
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+def load_custom_data(
+    corpus_path:str,
+    query_path:str,
+    qrels_path:str
+):
+    corpus, queries, qrels = GenericDataLoader(
+                                    corpus_file=corpus_path, 
+                                    query_file=query_path, 
+                                    qrels_file=qrels_path).load_custom()
+    
+    return corpus, queries, qrels
+
+def evaluate_sentence_transf(
+    model_name:str,
+    corpus:object,
+    queries:object,
+    qrels:object
+):
+    model = DRES(models.SentenceBERT(model_name))
+    retriever = EvaluateRetrieval(model, score_function="dot")
+    results = retriever.retrieve(corpus, queries)
+    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+    return ndcg, _map, recall, precision
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", nargs='?', default="queries") ### With this param we select the kind of query: only the BDI item tite, the firs question, etc.
+    args = parser.parse_args()
+    corpus,queries,qrels = load_custom_data("../dataset_format_beir/sentences.jsonl", "../dataset_format_beir/"+str(args.query)+".jsonl", "../dataset_format_beir/qrels.tsv")
+    sr_model = 'all-mpnet-base-v2'
+    ndcg, _map, recall, precision = evaluate_sentence_transf(sr_model, corpus, queries, qrels)
+    with open("../baselines/semantic-search-retrieval.txt",'a+') as f:
+        print("Ndcg:", ndcg, "MAP:", _map, "Recall:", recall, "Precision:", precision)
+        f.write("Model:"+sr_model+"\nNdcg:"+ json.dumps(ndcg)+ " MAP:"+ json.dumps(_map) + " Recall:"+ json.dumps(recall) + " Precision:"+ json.dumps(precision))
